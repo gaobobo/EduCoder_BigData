2938301ad4a2cbf5bd3c6ecbfe3ffb2f57c66b9c
Bobby Gao <32958058+gaobobo@users.noreply.github.com>
2025-03-17 10:55:32 +0800

    fix(HDFS-2): add punctuation for echo

--- scripts/HDFS-2.2.sh
+++ scripts/HDFS-2.2.sh
@@ -6,7 +6,7 @@ force_pass() {
 
 normal_pass() {
 
-    echo -e "\e[38;5;11;7m 警告: \e[0mHDFS有概率因为超时问题停机导致无法百分百通关，且start-dfs.sh内有exit脚本无法直接调用" \
+    echo -e "\e[38;5;11;7m 警告: \e[0mHDFS有概率因为超时问题停机导致无法百分百通关，且start-dfs.sh内有exit脚本无法直接调用。" \
         "将自动使用force模式通关，这会修改评测系统的内核。"
 
     force_pass
